ce4e5c6faba445f7f31adc85128a24217b1f195d
sebaztyn <sebastinechima@gmail.com>
2020-10-08 03:44:06 +1200

    use XHR

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
+import uploadVideo from "../../utils/uploadVideo";
 
 const Videos = () => {
   const onFileChange = async (e) => {
     const file = e.target.files[0];
-    console.log("file", file.name);
-    // const formdata = new FormData();
-    // formdata.append();
     const data = await fetch("http://localhost:5000/upload", {
       method: "POST",
       body: JSON.stringify({ filename: file.name }),
@@ -14,17 +12,19 @@ const Videos = () => {
       },
     });
     const parsedData = await data.json();
-    const formData = new FormData();
-    Object.keys(parsedData.fields).forEach((data) => {
-      formData.append(data, parsedData.fields[data]);
-    });
-    formData.append("file", file);
-    const uploadedData = await fetch(parsedData.url, {
-      method: "POST",
-      body: formData,
-    });
-    const parsedUpload = await uploadedData.json();
-    console.log("data :>> ", parsedUpload.Location);
+    const uploadedResult = await uploadVideo({ parsedData, file });
+    console.log("uploadedResult :>> ", uploadedResult);
+    // const formData = new FormData();
+    // Object.keys(parsedData.fields).forEach((data) => {
+    //   formData.append(data, parsedData.fields[data]);
+    // });
+    // formData.append("file", file);
+    // const uploadedData = await fetch(parsedData.url, {
+    //   method: "POST",
+    //   body: formData,
+    // });
+    // const parsedUpload = await uploadedData.json();
+    // console.log("data :>> ", parsedUpload.Location);
   };
   return (
     <div>
---
+++ src/utils/uploadVideo.js
@@ -0,0 +1,31 @@
+const uploadVideo = ({ parsedData, file }) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData();
+    Object.keys(parsedData.fields).forEach((data) => {
+      formData.append(data, parsedData.fields[data]);
+    });
+    formData.append("file", file);
+    xhr.open("POST", parsedData.url);
+    xhr.responseType = "json";
+    xhr.onerror = (error) => {
+      reject(error);
+    };
+    xhr.onload = () => {
+      if (xhr.status > 200 && xhr.status < 300) {
+        resolve({ ...xhr.response, url: xhr.getResponseHeader("Location") });
+      } else {
+        reject({
+          status: xhr.status,
+          message: xhr.statusText,
+        });
+      }
+    };
+    xhr.upload.onprogress = function (event) {
+      console.log(`Uploaded ${event.loaded} of ${event.total} bytes`);
+    };
+    xhr.send(formData);
+  });
+};
+
+export default uploadVideo;
