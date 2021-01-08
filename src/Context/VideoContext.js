958af944bb0edc171e6ed0db6e138ce7a3d604c1
Chima Ekeneme <sebastinechima@gmail.com>
2021-01-08 18:24:02 +0100

    Add a video cancel button

--- src/Context/VideoContext.js
+++ src/Context/VideoContext.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { createContext, useState } from 'react';
-
 export const videoContext = createContext();
+
 const VideoContextProvider = ({ children }) => {
+  const cancelToken = axios.CancelToken;
+  const [abortSource, setAbortSource] = useState(cancelToken.source());
   const [uploadProgress, setUploadProgress] = useState(0);
   const [uploadResult, setUploadResult] = useState(null);
   const [isUploading, setIsUploading] = useState(false);
@@ -20,13 +22,13 @@ const VideoContextProvider = ({ children }) => {
   const fileTitleHandler = (evt) => {
     setVideoTitle(evt.target.value);
   };
-  const cancelTokenSource = axios.CancelToken.source();
   const cancelUploadHandler = (evt) => {
-    console.log('We are cancelling>>>>>>');
-    cancelTokenSource.cancel();
+    // console.log('We are cancelling>>>>>>');
+    abortSource.cancel('Video upload cancelled');
   };
   const uploadData = async ({ parsedData, file }) => {
     setIsUploading(() => true);
+    // setAbortSource(() => cancelToken.source());
     try {
       const formData = new FormData();
       Object.keys(parsedData.fields).forEach((data) => {
@@ -38,7 +40,7 @@ const VideoContextProvider = ({ children }) => {
         data: formData,
         method: 'POST',
         headers: { 'Content-Type': 'multipart/form-data' },
-        cancelToken: cancelTokenSource.token,
+        cancelToken: abortSource.token,
         onUploadProgress: (evt) => {
           setUploadProgress(Math.floor((evt.loaded * 100) / evt.total));
         },
@@ -46,7 +48,9 @@ const VideoContextProvider = ({ children }) => {
       setIsUploading(() => false);
       setUploadResult(result);
     } catch (error) {
-      setIsUploading(() => false);
+      setIsUploading(false);
+      setAbortSource(cancelToken.source());
+      setUploadProgress(0);
       console.log('uploadError :>> ', error);
     }
   };
--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -110,13 +110,13 @@ const Videos = () => {
             >
               Upload Video
             </button>
-            {/* <button
+            <button
               type='button'
               className='cancel-button'
               onClick={cancelUploadHandler}
             >
               Cancel upload
-            </button> */}
+            </button>
           </div>
         </VideoSection>
       </VideoContainer>
